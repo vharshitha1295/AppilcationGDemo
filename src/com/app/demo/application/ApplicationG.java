5e61b20fda29cfa13c1e28b70aa9cf6a134616c5
vharshitha1295 <harshithav1295@gmail.com>
2023-12-28 17:29:36 -0500

    My First Application G Demo

---
+++ src/com/app/demo/application/ApplicationG.java
@@ -0,0 +1,10 @@
+package com.app.demo.application;
+
+public class ApplicationG {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Application G");
+	}
+
+}
