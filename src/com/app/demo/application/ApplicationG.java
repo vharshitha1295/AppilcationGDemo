6e70d3ea2d42755035e7141baa74f662694b7861
vharshitha1295 <harshithav1295@gmail.com>
2023-12-28 22:17:20 -0500

    Added changes

--- src/com/app/demo/application/ApplicationG.java
+++ src/com/app/demo/application/ApplicationG.java
@@ -5,7 +5,7 @@ public class ApplicationG {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Application G");
-		System.out.println("Hello Harshitha");
+		System.out.println("Hello Harshitha Nandeep");
 	}
 
 }
